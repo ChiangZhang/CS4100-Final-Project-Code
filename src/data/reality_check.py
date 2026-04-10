944fac054aa75bfe92236e185fe217e6ba2b84d4
Deeptha Jayaraman <deepthajay@gmail.com>
2026-04-10 17:35:50 -0400

    select 5k tracks to label that covers more genres

--- src/data/reality_check.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-labels = pd.read_csv('src/data/validation_5k_labeled.csv')
-features = pd.read_csv('data/eda/dataset.csv') # Use the raw file to check genres
-
-# Merge briefly to see the genre spread
-check_df = pd.merge(labels[['track_id']], features[['track_id', 'track_genre']], on='track_id')
-print(check_df['track_genre'].value_counts())
-print(f"Unique genres in training set: {check_df['track_genre'].nunique()}")
