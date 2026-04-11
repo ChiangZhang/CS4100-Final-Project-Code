680250d02563c06cf1dd60710206c4e4fab10e42
Deeptha Jayaraman <deepthajay@gmail.com>
2026-04-11 12:49:11 -0400

    updated track_servicepy to pull track_details from api

--- src/services/track_service.py
+++ src/services/track_service.py
@@ -33,9 +33,42 @@ class TrackService:
         except:
             return None
         
+    @staticmethod
+    def get_track_details(reccobeats_id):
+        try:
+            url = f"https://api.reccobeats.com/v1/track/{reccobeats_id}"
+            headers = {'Accept': 'application/json'}
+            response = requests.get(url, headers=headers)
+            response_json = response.json()
+            
+            # Extract artists and join them with a semicolon to match CSV
+            artists_list = response_json.get("artists", [])
+            if artists_list:
+                artists_str = ";".join([artist.get("name", "Unknown Artist") for artist in artists_list])
+            else:
+                artists_str = "Unknown Artist"
+            
+            track_info = {
+                "track_name": response_json.get("name", "Unknown Track"),
+                "artists": artists_str # Changed to plural "artists"
+            }
+
+            return pd.DataFrame([track_info])
+
+        except:
+            return None
+        
     def get_song_features(self, spotify_url):
         spotify_track_id = TrackService.parse_spotify_track_id(spotify_url)
         reccobeats_track_id = TrackService.get_reccobeats_id(spotify_track_id)
+
         if reccobeats_track_id is None:
             return None
-        return TrackService.get_audio_features(reccobeats_track_id)
+        
+        features_df = self.get_audio_features(reccobeats_track_id)
+        details_df = self.get_track_details(reccobeats_track_id)
+        
+        if features_df is not None and details_df is not None:
+            return pd.concat([details_df, features_df], axis=1)
+            
+        return None
